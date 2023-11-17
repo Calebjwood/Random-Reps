e8c8ec51c467375b40d07890ca3012efd1d81388
Owen Lang <owen.lang146@gmail.com>
2023-11-16 19:24:42 -0600

    workout data and typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,6 +4,29 @@ const typeDefs = `
     firstName: String
     lastName: String
     email: String
+    workouts: [Workout]
+    followers: Int
+    following: [User]
+    followingCount: Int
+  }
+
+  type Exercise {
+    _id: ID
+    title: String
+    types: [String]
+    sets: Int
+    reps: Int
+    rest: Int
+    link: String
+    time: Int
+  }
+
+  type Workout {
+    _id: ID
+    title: String
+    type: [String]
+    exercises: [Exercise]
+    estimatedTime: Int
   }
 
   type Auth {
@@ -12,11 +35,16 @@ const typeDefs = `
 
   type Query {
     user: User
+    getWorkout: Workout
+    getFollowing: [User]
+    searchUsers: [User]
   }
 
   type Mutation {
     signin(firstName: String!, lastName: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
+    saveWorkout(title: String!, type: [String!], exercises: [Exercise!]): Workout
+    follow(_id: Int!): User
   }
 `;
 
