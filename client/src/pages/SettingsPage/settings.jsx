909b9f84c81814c07c5dfa7b6409891db7f75516
Rafael Gomez <rgomez2014@gmail.com>
2023-11-21 19:01:50 -0600

    added states

--- client/src/pages/SettingsPage/settings.jsx
+++ client/src/pages/SettingsPage/settings.jsx
@@ -1,12 +1,9 @@
 import './style.scss';
 import { useState } from 'react';
-import { Container, Form, Button } from 'react-bootstrap'
-import { Link } from 'react-router-dom';
-import exercisesData from '../../seeds/exercises.json'
+import { Container, Form, Button } from 'react-bootstrap';
+import exercisesData from '../../seeds/exercises.json';
 
-const getSeedData = () => {
-  return exercisesData;
-};
+const getSeedData = () => exercisesData;
 
 const getUniqueTypes = () => {
   const types = new Set();
@@ -18,9 +15,23 @@ const getUniqueTypes = () => {
 
 function Settings() {
   const [checkedId, setCheckedId] = useState('');
+  const [selectedTypes, setSelectedTypes] = useState(new Set());
+  const navigate = useNavigate();
+  
   const handleCheckboxChange = (event) => {
-    const id = event.target.id;
-    setCheckedId(checkedId === id ? '' : id);
+    setCheckedId(event.target.id);
+  };
+
+  const handleTypeChange = (event) => {
+    const type = event.target.id;
+    setSelectedTypes(prevTypes => new Set(prevTypes).add(type));
+  };
+
+  const handleGenerateWorkout = () => {
+    console.log('Generating workout with:', { duration: checkedId, types: Array.from(selectedTypes) });
+    alert('Workout generation is under development.');
+    navigate('/workouts', { state: { duration: checkedId, types: Array.from(selectedTypes) } })
+    // Add logic to generate workout and navigate to the next page
   };
 
   const workoutTypes = getUniqueTypes();
@@ -29,37 +40,36 @@ function Settings() {
     <Container>
       <Form>
         <Form.Group>
-        <Form.Label htmlFor="duration">Duration</Form.Label>
-        {['short', 'medium', 'long'].map((id) => (
-          <div key={id} className="mb-3">
-            <Form.Check
-              type='checkbox'
-              id={id}
-              label={id.charAt(0).toUpperCase() + id.slice(1)}
-              onChange={handleCheckboxChange}
-              checked={checkedId === id}
-              disabled={checkedId !== '' && checkedId !== id}
-            />
-          </div>
-        ))}
+          <Form.Label htmlFor="duration">Duration</Form.Label>
+          {['short', 'medium', 'long'].map((id) => (
+            <div key={id} className="mb-3">
+              <Form.Check
+                type='radio'
+                id={id}
+                label={id.charAt(0).toUpperCase() + id.slice(1)}
+                onChange={handleCheckboxChange}
+                checked={checkedId === id}
+              />
+            </div>
+          ))}
         </Form.Group>
         <Form.Group>
-        <Form.Label htmlFor="exercise-types">Types of Exercises</Form.Label>
-        {workoutTypes.map((type) => (
-          <div key={type} className="mb-3">
-            <Form.Check
-              type='checkbox'
-              id={type}
-              label={type}
-            />
-          </div>
-        ))}
+          <Form.Label htmlFor="exercise-types">Types of Exercises</Form.Label>
+          {workoutTypes.map((type) => (
+            <div key={type} className="mb-3">
+              <Form.Check
+                type='checkbox'
+                id={type}
+                label={type}
+                onChange={handleTypeChange}
+                checked={selectedTypes.has(type)}
+              />
+            </div>
+          ))}
         </Form.Group>
-         <Button variant="primary" size="lg" active>
-          <Link to="/"  style={{ color: 'inherit', textDecoration: 'inherit'}}>
-        Generate Workout
-        </Link>
-      </Button>{' '}
+        <Button variant="primary" size="lg" onClick={handleGenerateWorkout}>
+          Generate Workout
+        </Button>
       </Form>
     </Container>
   );
