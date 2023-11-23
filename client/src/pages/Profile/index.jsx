d95d2c9c53e506f6d8c80378bd592cfb4b66c6c8
Rafael Gomez <rgomez2014@gmail.com>
2023-11-23 09:56:59 -0600

    User context created

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -1,26 +1,21 @@
-import { useQuery } from "@apollo/client";
-
-import { QUERY_USER } from "../../utils/queries";
-
 import { Container, Row, Col, Navbar } from "react-bootstrap";
 import ActivityFeed from "../../components/ActivityFeed";
+import { useUserContext } from "../../utils/user-context";
 import "./style.scss";
 
 export default function Profile() {
-  const { data, loading } = useQuery(QUERY_USER);
-
-  console.log(data);
-
+  const { user, loading } = useUserContext();
+  console.log(user)
   return (
     <div id="profile-page">
       {loading && <h2 className="loading-data">Loading user data...</h2>}
 
-      {data?.user && (
+      {!loading && (
         <div>
           <Container>
             <Row>
               <Col className="d-flex align-items-center">
-                <h3>{data.user.username}</h3>
+                <h3>{user.username}</h3>
               </Col>
               <Col className="d-flex align-items-center">
                 <h3>Followers</h3>
@@ -44,7 +39,7 @@ export default function Profile() {
               <Col></Col>
               <Col lg={10} className="border">
                 <ActivityFeed
-                  workouts={data.user.savedWorkouts}
+                  workouts={user.savedWorkouts}
                   title="Activity Feed:"
                 />
               </Col>
