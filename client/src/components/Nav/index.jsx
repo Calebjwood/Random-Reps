646286364164276251468023384d606837cf9beb
Rafael Gomez <rgomez2014@gmail.com>
2023-11-27 18:50:37 -0600

    Search

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom";
-
+import { Form, Button } from "react-bootstrap";
+import { useState } from "react";
+import { SEARCH_USERS } from "../../utils/queries"
 import Auth from "../../utils/auth";
-
 import logo from '../../assets/images/logo.png';
-
 import './style.scss';
+import { useQuery } from "@apollo/client";
 
-export default function Nav() {
+const Nav = () => {
+  const [search, setSearch ] = useState('')
+  const { data, loading } = useQuery(SEARCH_USERS, {variables: {username:search}})
+  const handleClick = (e) => {
+    e.preventDefault()
+    console.log(data)
+  }
 
   return (
     <header>
@@ -16,17 +23,30 @@ export default function Nav() {
 
       <div>
         <nav>
-              <Link to="/">Home</Link>
-              <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
-              {Auth.loggedIn() && (
-              <>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+          <Form className="d-flex ml-auto" style={{ paddingLeft: '1rem' }} >
+            <Form.Control
+              type="search"
+              placeholder="Search Username"
+              className="me-2"
+              aria-label="Search"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <Button variant="outline-success" type="submit" onClick={(e) => handleClick(e) }>Search </Button>
+          </Form>
+          {Auth.loggedIn() && (
+            <>
               <Link to="/profile">Profile</Link>
               <div className="logout-link" onClick={() => Auth.logout()}>Logout</div>
-              </>
-              )}
+            </>
+          )}
         </nav>
       </div>
     </header>
   );
 }
+
+export default Nav
