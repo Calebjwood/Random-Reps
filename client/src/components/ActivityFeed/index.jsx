282768a4bebb52675fb4b40b1825bcd5b91d15b6
Rafael Gomez <rgomez2014@gmail.com>
2023-11-28 19:58:54 -0600

    Added link to activity feed

--- client/src/components/ActivityFeed/index.jsx
+++ client/src/components/ActivityFeed/index.jsx
@@ -1,19 +1,17 @@
 import { Link } from 'react-router-dom';
 
-
 export default function ActivityFeed({ workouts, title }) {
     if(!workouts.length){
         return <h3>No Workouts saved</h3>
     }
-
     return(
         <div>
             <h1 className="text-center">{title}</h1>
             {workouts.map((workout) => (
             <div key={workout._id} >
                 <h3>{workout.title}</h3>
-                {workout.exercises.map((exercises) => (
-                <h5>{exercises.title} <br />
+                {workout.exercises.map((exercises, index) => (
+                <h5 key={index}>{exercises.title} <br />
                     {exercises.sets} <br />
                     {exercises.reps} <br />
                     {exercises.rest} <br />
@@ -21,12 +19,11 @@ export default function ActivityFeed({ workouts, title }) {
                   </h5>
                 )
                 )}
-            <Link to="/settings" >
-            <button className="btn btn-danger">start workout</button>
+            <Link to={`/workout/${workout._id}`} >
+            <button className="btn btn-danger">Start Workout</button>
                 </Link>
             </div>
             ))
-
             }
         </div>
     )
