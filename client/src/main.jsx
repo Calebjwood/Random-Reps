82d9edb1ae901bad9bd0848266a07abb907cc813
Rafael Gomez <rgomez2014@gmail.com>
2023-11-28 19:57:34 -0600

    added route for workoutID

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -43,6 +43,10 @@ const router = createBrowserRouter([
         element:<Workout />
       },
       {
+        path:'/workout/:workoutId',
+        element: <Workout />
+      },
+      {
         path:'/profile/:userId',
         element: <Profile />
       }
